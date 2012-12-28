f2a8fd077268d645a3d3d2f8e260e5060ad1e567
Ben Ripkens <bripkens.dev@gmail.com>
2012-12-28 11:27:04 +0100

    Fixed navigation issues

--- src/js/controller/MovieOverviewCtrl.js
+++ src/js/controller/MovieOverviewCtrl.js
@@ -5,6 +5,10 @@ function MovieOverviewCtrl($scope, $location) {
     {title: "Star Wars", id: "5270ca09-03ee-4677-8d4c-91eac3c1821d"},
     {title: "Forrest Gump", id: "4dd66d58-922c-482b-af62-c8df6748ccd6"}
   ];
+
+  $scope.click = function(movieId) {
+    $location.path("/movies/" + movieId);
+  };
 }
 
 MovieOverviewCtrl.$inject = ["$scope", "$location"];
