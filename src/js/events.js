cfc7ec018c14c5a47d6cea95eb0f771bb2ad47ab
Ben Ripkens <bripkens.dev@gmail.com>
2013-01-08 23:30:40 +0100

    Removed superfluous event.

--- src/js/events.js
+++ src/js/events.js
@@ -2,13 +2,9 @@ define(["lodash"], function(_) {
   "use strict";
 
   var eventPrefix = "event";
-
   var id = _.uniqueId.bind(_, eventPrefix);
 
   return {
-    // fn(newTitle)
-    pageTitle: id(),
-
     tags: {
       // fn()
       update: id()
